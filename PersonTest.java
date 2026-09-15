a4ab1c00847cfd6b9bfc7c1a3ba67d85d0fc1301
sarahisophia <quinteroval.sarah@student.ccm.edu>
2026-09-15 13:27:43 -0400

    work for lab 4

--- PersonTest.java
+++ PersonTest.java
@@ -1,3 +1,21 @@
-public class PersonTest {
+public class PersonTest 
+{
+    public static void main(String[] args) 
+    {
+        Person person1 = new Person();
+        person1.setName("");
+        person1.setAge(28);
+        person1.setEmail("");
+
+        Person person2 = new Person("Temp Name", 0, "temp email");
+
+        person2.setName("");
+        person2.setAge(34);
+        person2.setEmail("");
+
+        person1.displayInfo();
+        person2.displayInfo();
+    }
+        
+    }
     
-}
