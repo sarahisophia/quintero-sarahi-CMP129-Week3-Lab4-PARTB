726a67948a6a9a8115c5a46965b7bd99aea0dd69
sarahisophia <quinteroval.sarah@student.ccm.edu>
2026-09-15 13:27:13 -0400

    work for lab 4

--- Person.java
+++ Person.java
@@ -1,3 +1,50 @@
 public class Person {
-    
+    private String name;
+    private int age;
+    private String email;
+
+    public Person(String newName, int newAge, String newEmail) {
+        name = newName;
+        age = newAge;
+        email = newEmail;
+    }
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int newAge) {
+        if (newAge >= 0) {
+            age = newAge;
+        } else {
+            System.out.println("Age cannot be negative.");
+        }
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String newEmail) {
+        email = newEmail;
+    }
+
+    public void displayInfo() {
+        System.out.println("----- Person Details -----");
+        System.out.println("Name  : " + name);
+        System.out.println("Age   : " + age);
+        System.out.println("Email : " + email);
+        System.out.println("--------------------------\n");
+    }
 }
