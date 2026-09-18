d658f1480e3272fbeccc018c9cf34b185a759e77
sarahisophia <quinteroval.sarah@student.ccm.edu>
2026-09-17 22:25:54 -0400

    work for lab 4

--- PersonTest.java
+++ PersonTest.java
@@ -3,15 +3,15 @@ public class PersonTest
     public static void main(String[] args) 
     {
         Person person1 = new Person();
-        person1.setName("");
+        person1.setName("Sarahi quintero");
         person1.setAge(28);
-        person1.setEmail("");
+        person1.setEmail("quintero@gmail.com");
 
-        Person person2 = new Person("Temp Name", 0, "temp email");
+        Person person2 = new Person("Bob", 87, "bob87@gmail.com");
 
-        person2.setName("");
+        person2.setName("donald Junior");
         person2.setAge(34);
-        person2.setEmail("");
+        person2.setEmail("donaldJunior@gmail.com");
 
         person1.displayInfo();
         person2.displayInfo();
